8dc58c19380d17ae5a1e9c4386529e6b93521f3b
gscottqueen <scott.queen@bixal.com>
2020-08-26 10:37:59 -0400

    updated formating

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,7 @@ const Header = ({ siteTitle }) => (
   <header>
     <div>
       <h1>
-        <Link to="/" >
-          {siteTitle}
-        </Link>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
