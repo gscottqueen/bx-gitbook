bd6021a24759f8333bc5b61bc2d1f44f7b310ee6
gscottqueen <scott.queen@bixal.com>
2020-08-26 10:14:34 -0400

    more defaulting

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,17 +26,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Bixal Techbook`,
+        short_name: `Bixal Techbook`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/bixal-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bixal-icon.jpg`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
