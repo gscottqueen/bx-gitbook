3602b8414b4400886df5fdcf3426b3f1f8775ef6
gscottqueen <scott.queen@bixal.com>
2020-08-27 16:28:27 -0400

    includes GitEditButton in our page template

--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -1,16 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import GitEditButton from "../components/gitedit"
 
 export default function PageTemplate({ data }) {
   const { markdownRemark } = data
-  const { headings, html } = markdownRemark
+  const { headings, html, fileAbsolutePath} = markdownRemark
+
+  // build our github rel directory
+  const regexDir = /.*(\/bx-techbook)+/
+  const filePath = `${fileAbsolutePath}`
+  const gitFile = filePath.replace(regexDir, '')
 
   return (
     <Layout>
       <div className="page-container">
         <div className="page-markdown">
           <h1>{headings.value}</h1>
+          <GitEditButton gitFile={gitFile}/>
           <div
             className="page-markdown-content"
             dangerouslySetInnerHTML={{ __html: html }}
