eef9242b353aa0e70cee37058f6c63e0255d2969
gscottqueen <scott.queen@bixal.com>
2020-09-10 15:46:30 -0400

    updated ref

--- src/templates/PageTemplate.js
+++ src/templates/PageTemplate.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import GitEditButton from "../components/GitEdit"
 import PrevNext from "../components/PrevNext"
 import InPageNav from "../components/InPageNav"
