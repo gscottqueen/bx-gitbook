2813b1c02aa1c77358ff296dee1f5b519dc2b729
gscottqueen <geoffreysqueen@gmail.com>
2021-01-26 11:49:01 -0500

    remove nav from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import React from "react"
 import Logo from './Logo'
-import Nav from "./Nav"
 
-const Header = () => (
-  <header style={{"display": "flex"}} id="header">
-    <div>
-      <h1><Logo/></h1>
-    </div>
-    <a href="#menu">Menu</a>
-    <Nav />
-  </header>
-)
+export default function Header() {
 
-export default Header
+  return (
+    <header style={{"display": "flex"}} id="header">
+      <div>
+        <h1><Logo/></h1>
+      </div>
+      <a href="#menu">Menu</a>
+    </header>
+  )
+}
