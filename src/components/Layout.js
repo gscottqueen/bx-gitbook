0f71d1f11157d494dbbe7969ad42945d1b319ca2
gscottqueen <geoffreysqueen@gmail.com>
2021-01-26 11:49:30 -0500

    move nav into layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./Header"
 import { SkipNavLink } from "@reach/skip-nav";
 import "@reach/skip-nav/styles.css";
+import Nav from '../components/Nav'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -28,6 +29,40 @@ const Layout = ({ children }) => {
     <SkipNavLink />
     <div id="page">
       <Header siteTitle={data.site.siteMetadata.title} />
+      <Nav/>
+      {/* <Navi
+          id="menu"
+          options={{
+            "extensions": [
+              "pagedim-black"
+            ]
+          }}
+        >
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/work">Our work</a></li>
+            <li><span>About us</span>
+              <ul>
+                <li><a href="/about/history">History</a></li>
+                <li><span>The team</span>
+                  <ul>
+                    <li><a href="/about/team/management">Management</a></li>
+                    <li><a href="/about/team/sales">Sales</a></li>
+                    <li><a href="/about/team/development">Development</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li><span>Services</span>
+              <ul>
+                <li><a href="/services/design">Design</a></li>
+                <li><a href="/services/development">Development</a></li>
+                <li><a href="/services/marketing">Marketing</a></li>
+              </ul>
+            </li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </Navi> */}
       <section className="content">
         <main>{children}</main>
         <aside/>
